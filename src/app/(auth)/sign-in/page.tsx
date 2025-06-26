5b8641980c377f5542edbd06aacd3d817ace360e
yuvrajSingh-rajput <sunilkumarsingh6307@gmail.com>
2025-06-26 12:11:26 +0530

    feat: implemented oAuth and protected the auth routes

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
+import { auth } from "@/lib/auth";
 import SignInView from "@/modules/auth/ui/views/sign-in-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Signin = () => {
+const Signin = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
+
   return <SignInView />;
 };
 
---
+++ src/modules/home/ui/views/home-view.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+export const HomeView = () => {
+    const router = useRouter();
+  const  {data: session} = authClient.useSession();
+  return (
+    <div className="flex flex-col p-4 gap-y-4">
+      <p>Logged in as {session?.user.name}</p>
+      <Button onClick={() => authClient.signOut({
+        fetchOptions: {
+            onSuccess: () => router.push("/sign-in")
+        }
+      })}>
+        Sign Out
+      </Button>
+      
+    </div>
+  )
+};
