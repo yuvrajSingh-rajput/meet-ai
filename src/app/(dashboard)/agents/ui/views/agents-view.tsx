deb35af2d1524208fb67d0789b077b00052716cb
yuvrajSingh-rajput <sunilkumarsingh6307@gmail.com>
2025-06-28 13:52:15 +0530

    Created a responsive table to display the agents on the user's dashboard

--- src/app/(dashboard)/agents/ui/views/agents-view.tsx
+++ src/app/(dashboard)/agents/ui/views/agents-view.tsx
@@ -4,14 +4,21 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { DataTable } from "@/modules/agents/ui/components/data-table";
+import { columns } from "@/modules/agents/ui/components/columns";
+import { EmptyState } from "@/components/empty-state";
+
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <div>
-      {JSON.stringify(data, null, 2)}
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 && (
+        <EmptyState title="Create your first agent" description="create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call." />
+      )}
     </div>
   );
 };
---
+++ src/components/empty-state.tsx
@@ -0,0 +1,19 @@
+import { AlertCircleIcon } from "lucide-react";
+import Image from "next/image";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+export const EmptyState = ({ title, description }: Props) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+        <Image src="/empty.svg" alt="Empty" width={240} height={240} />
+      <div className="flex flex-col gap-y-6 max-w-md mx-auto text-center">
+        <h6 className="text-lg font-medium">{title}</h6>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </div>
+    </div>
+  );
+};
